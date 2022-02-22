b8df245b7cabb79e8ecd9d171cb864741b05c7b7
Hemapriya14 <99642541+Hemapriya14@users.noreply.github.com>
2022-02-22 22:08:20 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,19 @@
+package org.college;
+
+public class College {
+	public void collegeName() {
+		System.out.println("Annauniversity");
+	} 
+	public void collegeCode() {
+		System.out.println("5133");
+	}
+	public void collegeRank() {
+		System.out.println("12");
+	}
+	public static void main(String[] args) {
+		College stu = new College();
+		stu.collegeName();
+
+	}
+
+}
